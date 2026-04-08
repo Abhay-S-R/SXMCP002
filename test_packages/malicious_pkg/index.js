78a4524dd8bd55c4a45b48260c9e4284f82f0e87
Rohith-Saran <rohithsaran070@gmail.com>
2026-04-08 22:35:49 +0530

    merge 1

--- test_packages/malicious_pkg/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  encrypt: function(data) {
-    return Buffer.from(data).toString('base64');
-  },
-  decrypt: function(data) {
-    return Buffer.from(data, 'base64').toString('utf8');
-  }
-};
